f88f3c5559a176aa9a3f4e0b01c2f121caa9628e
Cerlin <cerlin@linkstreet.in>
2021-07-24 00:05:35 +0530

    Homepage test

--- src/main/java/com/qa/pages/Homepage.java
+++ src/main/java/com/qa/pages/Homepage.java
@@ -1,9 +1,59 @@
 package com.qa.pages;
 
-public class Homepage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-	public void homepage() {
-		System.out.println("Homepage");
+import com.qa.utils.BaseClass;
+
+public class Homepage {
+	
+	@FindBy(xpath = "//*[text() ='New Customer']")
+	WebElement newcustomerbtn;
+	
+	@FindBy(name = "name")
+	WebElement customername;
+
+	@FindBy(xpath = "//input[@name='rad1'][2]")
+	WebElement sex;
+
+	@FindBy(id = "dob")
+	WebElement DOB;
+
+	@FindBy(name = "addr")
+	WebElement address;
+
+	@FindBy(name = "city")
+	WebElement city;
+
+	@FindBy(name = "state")
+	WebElement state;
+
+	@FindBy(name = "pinno")
+	WebElement pin;
+
+	@FindBy(name = "telephoneno")
+	WebElement tele;
+
+	@FindBy(name = "emailid")
+	WebElement emailid;
+
+	@FindBy(name = "sub")
+	WebElement submit;
+
+	public void newCustomer() {
+		
+		newcustomerbtn.click();
+
+		BaseClass.waitV_C(customername).sendKeys("Jamuna1712");
+		BaseClass.waitV_C(sex).click();
+		BaseClass.waitE_C(DOB).sendKeys("06-09-1990");
+		BaseClass.waitV_C(address).sendKeys("89, 10th Cross, 1 st main Jagadish nagar, Bangalore");
+		BaseClass.waitV_C(city).sendKeys("Bangalore");
+		BaseClass.waitE_C(state).sendKeys("Karnataka");
+		BaseClass.waitV_C(pin).sendKeys("560075");
+		BaseClass.waitV_C(tele).sendKeys("9812345678");
+		BaseClass.waitE_C(emailid).sendKeys("jamuan123@gmail.com");
+		BaseClass.waitE_C(submit).click();
 	}
 
 }
--- src/test/java/com/qa/tests/HomepageTest.java
+++ src/test/java/com/qa/tests/HomepageTest.java
@@ -1,12 +1,45 @@
 package com.qa.tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import com.qa.pages.Homepage;
+import com.qa.pages.Loginpage;
+import com.qa.utils.BaseClass;
+import com.qa.utils.Config;
+
 import org.testng.annotations.Test;
 
 public class HomepageTest {
+	
+	WebDriver driver;
+	Homepage home;
+	Loginpage login;
+	
+	@BeforeClass
+	public void setup() {
+		driver = BaseClass.initialization(Config.browser, Config.url);
+		login = PageFactory.initElements(driver, Loginpage.class);
+		home = PageFactory.initElements(driver, Homepage.class);
+	}
+	
+	
 	@Test
-	public void homepageTest() {
-		System.out.println("Homepage test");
+	public void loginTest() {
+		login.login(Config.adminuser, Config.adminpassword);
+	}
+	
+	@Test
+	public void newCustomerCreationTest() {
+		home.newCustomer();
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
 	}
 
 }
